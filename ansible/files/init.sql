7e9bf86d9e5e2d683c890f7348a31d352a10d9b5
Oleksii Lykov <oleksii.lykov@gmail.com>
2025-03-03 21:40:24 +0400

    test

---
+++ ansible/files/init.sql
@@ -0,0 +1,34 @@
+CREATE TABLE IF NOT EXISTS users (
+    id BIGINT PRIMARY KEY,
+    registration_date TIMESTAMP NOT NULL,
+    last_interaction TIMESTAMP,
+    lastname VARCHAR(255),
+    first_name VARCHAR(255),
+    phone VARCHAR(20),
+    country VARCHAR(255),
+    username VARCHAR(255),
+    bio TEXT
+);
+
+CREATE TABLE IF NOT EXISTS muscles (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS exercises (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    muscle INT REFERENCES muscles(id),
+    CONSTRAINT unique_exercise UNIQUE (name, muscle)
+);
+
+CREATE TABLE IF NOT EXISTS training (
+    id SERIAL PRIMARY KEY,
+    date TIMESTAMP NOT NULL,
+    user_id BIGINT REFERENCES users(id),
+    muscle_id INT REFERENCES muscles(id),
+    exercise_id INT REFERENCES exercises(id),
+    set INT,
+    weight DECIMAL(5, 2),
+    reps INT
+);
